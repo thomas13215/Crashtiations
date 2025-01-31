505dfc759a9a92f1ab68edc798cc6a2d5ae9e246
Johnpaul Gaviola <48337338+thomas13215@users.noreply.github.com>
2025-01-31 07:36:32 -0600

    Moved to main folder

--- Crashtiations-main/Crashtiations-main/Crashtiations-Dev/revolver_variables.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-#stuff for revolver blah blah fwhbnfewjnhkfekjnfwed
-var maxBullets = 6 # mag capacity
-var reserveAmmo = 24 #reserve ammo
-var curBullets = 6 # ammo in mag
-var reloading = false
